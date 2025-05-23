aeee89b286b7a6d126483fa012322a2862d9820b
Thong Dang <thong.dang@coderpush.com>
2025-05-23 11:15:34 +0700

    fix: add log to entrypoint, use yarn

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,25 @@ set -e
 export DATABASE_URL="postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 
 # Wait for PostgreSQL to be available
-until PGPASSWORD=$DB_PASSWORD psql -p $DB_PORT -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
+echo "Connecting to database..."
+
+until PGPASSWORD=$DB_PASSWORD psql -p "$DB_PORT" -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
   echo "PostgreSQL not ready, waiting..."
   sleep 2
 done
 
+echo "Database connected"
+
+
+echo "Pushing database..."
 # Run Prisma db push
 npx prisma db push
 
+echo "Generating Prisma client..."
 # Run Prisma generate
 SKIP_PRISMA_VERSION_CHECK=true npx prisma generate
+
+echo "Starting application..."
 
 # Start the application
 yarn start
